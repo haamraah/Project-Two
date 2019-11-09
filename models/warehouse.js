5a0a8ebf497a3fa9a98313e2fecb7233b3b49c1e
Aaron Beck <aaronmicalbeck@gmail.com>
2019-11-08 21:45:04 -0700

    created workorder and warehouse/materials models

---
+++ models/warehouse.js
@@ -0,0 +1,41 @@
+module.exports = function (sequelize, DataTypes) {
+    var Warehouse = sequelize.define("Warehouse", {
+
+        // name of material
+        materialName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 40]
+            }
+        },
+
+        // color/style of material
+        materialStyle: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 40]
+            }
+        },
+
+        // quantity of material
+        materialQuantity: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+
+        // price of material
+        materialPrice: {
+             type: DataTypes.DECIMAL(10,2),
+             allowNull: false,
+             validate: {
+                 len: [1]
+             }
+        }
+    });
+    return Warehouse;
+};
