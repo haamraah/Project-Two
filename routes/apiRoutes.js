d97392116cdb054484dc21a8d153ea2423bc0748
Shiyu Cheng <starkeyshiyu@gmail.com>
2019-11-12 19:45:38 -0700

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,4 @@
-var db = require("../models");
+const db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
@@ -9,10 +9,33 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.post("/dashboard/add", function(req, res) {
+    const data = {
+      creationDate = "10/10/1997",
+      clientNmae = "Shiyu",
+      clientPhone = 123456,
+      clientAddress = "550N 5AV, AZ",
+      jobAddress = "879E 7Th, Az",
+      installerName = "Bruce",
+      materials = "Light",
+      isComplete = "Pending",
+      comments = "Hello World!",
+    };
+
+    // let { creationDate, clientName, clientPhone, clientAddress, jobAddress, installerName, materials, isComplete, comments} = req.body;
+    // Insert into table
+    db.create({
+      creationDate,
+      clientName,
+      clientPhone,
+      clientAddress,
+      jobAddress,
+      installerName,
+      materials,
+      isComplete,
+      comments
+    }).then(job => res.redirect('/dashboard'))
+      .catch(err => console.log(err))
   });
 
   // Delete an example by id
