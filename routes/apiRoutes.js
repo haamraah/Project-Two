74b05fbac30746b73a6be3960c4184ccaf582eec
Shiyu Cheng <starkeyshiyu@gmail.com>
2019-11-13 21:05:26 -0700

    workorder POST after redirect to /dashborad page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,67 +1,36 @@
 var db = require("../models");
 
-module.exports = function (app) {
-  // Get all examples
-  // app.get("/api/examples", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.json(dbExamples);
-  //   });
-  // });
-
-  // // Create a new example
-  // app.post("/api/examples", function(req, res) {
-  //   db.Example.create(req.body).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
-
-  // // Delete an example by id
-  // app.delete("/api/examples/:id", function(req, res) {
-  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
-
+module.exports = function(app) {
   // work order routes
-  app.get("/api/workorder", function (req, res) {
-    db.Workorder.findAll({}).then(function (dbExamples) {
+  app.get("/api/workorder", function(req, res) {
+    db.Workorder.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new workorder
-  app.post("/api/workorder", function (req, res) {
-    db.Workorder.create(req.body).then(function (dbExample) {
-      res.json(dbExample);
-    });
+  app.post("/api/workorder", function(req, res) {
+    db.Workorder.create(req.body).then(res.redirect("/dashboard"));
   });
-  app.put("/api/workorder/:id", function (req, res) {
-
-    db.Workorder.update(
-      req.body, {
-        where: {
-          id: req.params.id
-        }
+  app.put("/api/workorder/:id", function(req, res) {
+    db.Workorder.update(req.body, {
+      where: {
+        id: req.params.id
       }
-    )
-      .then(function (rowsUpdated) {
-        res.json(rowsUpdated);
-      });
-
+    }).then(function(rowsUpdated) {
+      res.json(rowsUpdated);
+    });
   });
 
   // Delete an workorder by id
-  app.delete("/api/workorder/:id", function (req, res) {
+  app.delete("/api/workorder/:id", function(req, res) {
     db.Workorder.destroy({
       where: {
-
-
         id: req.params.id
       }
-    }).then(function (dbExample) {
+    }).then(function(dbExample) {
       res.json(dbExample);
     });
-
   });
 
   // test //
@@ -74,45 +43,35 @@ module.exports = function (app) {
 
   // Warehouse routes
 
-  app.get("/api/warehouse", function (req, res) {
-    db.Warehouse.findAll({}).then(function (dbExamples) {
+  app.get("/api/warehouse", function(req, res) {
+    db.Warehouse.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
-
     });
   });
 
   // Create a new warehouse
-  app.post("/api/warehouse", function (req, res) {
-    db.Warehouse.create(req.body).then(function (dbExample) {
+  app.post("/api/warehouse", function(req, res) {
+    db.Warehouse.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
-  app.put("/api/warehouse/:id", function (req, res) {
-
-    db.Warehouse.update(
-      req.body, {
-        where: {
-          id: req.params.id
-        }
+  app.put("/api/warehouse/:id", function(req, res) {
+    db.Warehouse.update(req.body, {
+      where: {
+        id: req.params.id
       }
-
-
-
-    )
-      .then(function (rowsUpdated) {
-        res.json(rowsUpdated);
-      });
-
+    }).then(function(rowsUpdated) {
+      res.json(rowsUpdated);
+    });
   });
   // Delete an warehouse by id
-  app.delete("/api/warehouse/:id", function (req, res) {
+  app.delete("/api/warehouse/:id", function(req, res) {
     db.Warehouse.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function (dbExample) {
+    }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
-
 };
